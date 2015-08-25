d69a8fde66fd1ae49320a30edabe453de8e91e3f
nicnys-8 <nirivonsiri@gmail.com>
2015-08-25 10:44:36 +0200

    First commit

---
+++ src/movierater/Movie.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movierater;
+
+/**
+ *
+ * @author nicnys
+ */
+public class Movie {
+    
+    public String title;
+    
+    public Movie(String title) {
+        this.title = title;
+    } 
+    
+    public void printTitle() {
+        System.out.println("Title of this cool movie: " + title);
+    }
+}
